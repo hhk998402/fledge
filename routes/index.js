697a7bf4036cf02708eabd6e991dce26871a927c
devil-Coder <raj.chandra2016@vitstudent.ac.in>
2018-03-31 17:25:48 +0530

    added new files

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+router.get('/twitter',(req,res,next)=>{
+    var stream = client.stream('statuses/filter', {track: 'javascript'});
+    stream.on('data', function(event) {
+        res.send(event && event.text);
+    });
+
+    stream.on('error', function(error) {
+        throw error;
+    });
+})
+
+router.get()
+module.exports = router;
